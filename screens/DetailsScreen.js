38ad2dfcd733872146951764bab7aa0c2f57e454
prajwalghate <prajwalghate18@gmail.com>
2021-06-16 13:15:38 +0530

    debug

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,104 +1,99 @@
 import React from 'react';
 import {ScrollView, Text} from 'react-native';
 
-export default function DetailsScreen({navigation, route,item}) {
+export default function DetailsScreen({navigation, route, item}) {
   const {vision, require, email, lookinfor} = route.params;
-  
-  console.log(email);
+
   console.log(require);
-  console.log(lookinfor);
   return (
-    <ScrollView style={{backgroundColor:'#fffafa',flex:1}}>
-         <Text
+    <ScrollView style={{backgroundColor: '#fffafa', flex: 1}}>
+      <Text
         style={{
           marginBottom: 3,
           marginTop: 35,
           marginHorizontal: 20,
           marginVertical: 5,
-          fontSize:25,
+          fontSize: 25,
           fontWeight: 'bold',
           textDecorationLine: 'underline',
         }}>
         My Vision:
       </Text>
       <Text
-      style={{
-          
+        style={{
           marginHorizontal: 20,
           marginVertical: 5,
-          fontSize:18,
-        }}
-      >{vision}</Text>
-       <Text
+          fontSize: 18,
+        }}>
+        {vision}
+      </Text>
+      <Text
         style={{
           marginBottom: 3,
           marginTop: 35,
           marginHorizontal: 20,
           marginVertical: 5,
-          fontSize:25,
+          fontSize: 25,
           fontWeight: 'bold',
           textDecorationLine: 'underline',
         }}>
         Looking For:
       </Text>
-     
-        {require.map((item) => (
-          <Text 
-           style={{
-          
-          marginHorizontal: 20,
-          marginVertical: 5,
-          fontSize:18,
-          }}
-          >{item.lable}
+
+      {require.map((item) => {
+        return (
+          <Text
+            style={{
+              marginHorizontal: 20,
+              marginVertical: 5,
+              fontSize: 18,
+              color: '#000',
+            }}>
+            {item.label}
           </Text>
-        ))}
-       
-      
-      
-      
-     
-   
-         <Text
+        );
+      })}
+
+      <Text
         style={{
           marginBottom: 3,
           marginTop: 35,
           marginHorizontal: 20,
           marginVertical: 5,
-          fontSize:25,
+          fontSize: 25,
           fontWeight: 'bold',
           textDecorationLine: 'underline',
         }}>
         Requirements:
       </Text>
-       <Text
+      <Text
         style={{
-          
           marginHorizontal: 20,
           marginVertical: 5,
-            fontSize:18,
-        }}
-       >{lookinfor}</Text> 
-         <Text
+          fontSize: 18,
+        }}>
+        {lookinfor}
+      </Text>
+      <Text
         style={{
           marginBottom: 3,
           marginTop: 35,
           marginHorizontal: 20,
           marginVertical: 5,
-          fontSize:25,
+          fontSize: 25,
           fontWeight: 'bold',
           textDecorationLine: 'underline',
         }}>
         My Contact Details:
       </Text>
       <Text
-       style={{
-          
+        style={{
           marginHorizontal: 20,
           marginVertical: 5,
-          fontSize:18,
-        }}
-      >{email}</Text> 
+          fontSize: 18,
+        }}>
+        {email}
+      </Text>
     </ScrollView>
   );
 }
